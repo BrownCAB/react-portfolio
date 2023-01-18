975136abc7551ba9708f785b36882bb3ed4ba38d
BrownCAB <brown.calah@hotmail.com>
2023-01-17 21:34:31 -0800

    Updates

---
+++ precache-manifest.e845cf35759471cc21939d0012ab3d93.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e90744e4bc521ea5587d",
+    "url": "/react-portfolio/static/css/main.f467186f.chunk.css"
+  },
+  {
+    "revision": "e90744e4bc521ea5587d",
+    "url": "/react-portfolio/static/js/main.b9cb04e8.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "e1ac88fad40cf53b4f1e",
+    "url": "/react-portfolio/static/js/2.9ee375ed.chunk.js"
+  },
+  {
+    "revision": "bdc160ee496e91bb035d8f0c2a654d16",
+    "url": "/react-portfolio/static/media/code_quiz.bdc160ee.png"
+  },
+  {
+    "revision": "68bb05d165589c1b23d1e1c8514db47c",
+    "url": "/react-portfolio/static/media/random_password_generator.68bb05d1.png"
+  },
+  {
+    "revision": "97c532569f0f2fe2a58a5b459162282e",
+    "url": "/react-portfolio/static/media/team_profile_generator.97c53256.png"
+  },
+  {
+    "revision": "e26d226972651337444baacf7626a8cd",
+    "url": "/react-portfolio/static/media/text-editor.e26d2269.png"
+  },
+  {
+    "revision": "6640e632bf4f005fe114a685f9a13f39",
+    "url": "/react-portfolio/static/media/weather_dashboard.6640e632.png"
+  },
+  {
+    "revision": "708dcae4dd4185bafbac69a55a7d5cf6",
+    "url": "/react-portfolio/static/media/work_scheduler.708dcae4.png"
+  },
+  {
+    "revision": "c95d79bb90384051a8a69d52231193fd",
+    "url": "/react-portfolio/index.html"
+  }
+];
